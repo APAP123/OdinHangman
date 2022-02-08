a0da9fcf08b504106f446ebb6db3e9d15ee9d64e
zork <adampapirnik@gmail.com>
2022-02-08 13:23:28 -0600

    Basic functionality.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,21 +1,38 @@
-word_list = File.read('google-10000-english-no-swears.txt')
-chosen_word = ''
+word_list = File.read('../google-10000-english-no-swears.txt')
+@chosen_word = ''
 letters_guessed = Array.new()
 wrong_guesses = 0
-correct_characters = Array.new()
+@correct_characters = Array.new()
 
 # Picks a random word from the provided list that matches the length criteria
 def pick_random_word
   word = ''
   while true
-    word = IO.readlines('google-10000-english-no-swears.txt')[rand(1..10000)].chomp
+    word = IO.readlines('../google-10000-english-no-swears.txt')[rand(1..10000)].chomp
     return word if word.length >= 5 && word.length <= 12
   end
 end
 
+# Verifies the entered character is a valid letter
 def valid_character?(char)
-  # TODO
+  return false if char.length != 1
+  return false unless char.match?(/[[:alpha:]]/)
+
   true
+end
+
+# Checks if guessed letter appears in word, then fills in accordingly. Returns false if letter does not appear
+def make_guess(letter)
+  correct_guess = false
+  word_array = @chosen_word.split(//)
+  word_array.each_with_index do |char, index|
+    puts "char: #{char}, index: #{index} letter: #{letter}"
+    if char == letter
+      correct_guess = true
+      @correct_characters[index] = letter
+    end
+  end
+  correct_guess
 end
 
 while true
@@ -23,12 +40,21 @@ while true
   break if gets.chomp.downcase == 'n'
 end
 
-chosen_word = pick_random_word
+@chosen_word = pick_random_word
+puts "Word is #{@chosen_word}"
+@correct_characters = Array.new(@chosen_word.length, '_')
 
 while true
-
-  puts 'Enter a letter!'
-
+  puts "Word so far: #{@correct_characters}"
+  puts "Letters guessed so far: #{letters_guessed}"
+  puts "Amount of incorrect guesses: #{wrong_guesses}"
+  puts 'Guess a letter, or enter @ to save your game.'
+  letter = gets.chomp.downcase
+  if valid_character?(letter)
+    wrong_guesses += 1 unless make_guess(letter)
+  else
+    puts 'Invalid character!'
+  end
 end
 
 # Player is first asked whether they'd like to load a game or start a new one
